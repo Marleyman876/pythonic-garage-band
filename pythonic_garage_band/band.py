f8bc94baa64e2261aa080e90233e9cc7260627c1
Marleyman876 <grant_garfield@yahoo.com>
2021-06-17 22:31:03 -0700

    passed all 18 test'

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -3,78 +3,67 @@ import re
 
 class Band:
     
-    all_band_mates = []
+    instances = []
     
     def __init__(self, name, members=None):
         self.name = name
         self.members = members
-        self.__class__.all_band_mates.append(self) #(https://stackoverflow.com/questions/9610993/python-type-or-class-or-is) found explnantion on how to use this on stack over flow in the demo roger user count class.count.
+        Band.instances.append(self)
 
     def __str__(self):
-      results = f"The band {self.name}" 
-      
-      return results 
-
+      return  f"The band {self.name}" 
+   
     def __repr__(self):
-        results = f'Band instance. name={self.name}, members={self.members}'
-        return results 
-    def solo(self):
-      output = ''
-      for person in self.members:
-        output += person.play_solo() + ' '
-        return output
+      return f'Band instance. name={self.name}, members={self.members}'
+    
+    def play_solos(self):
+      return [member.play_solo() for member in self.members]
+    
+    @classmethod
+    def to_list(cls):
+      return cls.instances  
+      
+       
+   
+      
+   
 
 class Musician:
-    def __init__(self, name, type, instrument, members=[]):
+    def __init__(self, name, type, instrument, solo):
       self.name = name
       self.type = type
       self.instrument = instrument
-      self.member = members
+      self.solo = solo
+  
   
     def __repr__(self):
       return f"{self.type} instance. Name = {self.name}"
    
     def __str__(self):
-      output = f"My name is {self.name} and I play {self.instrument}" 
-      return output
-    
-    def play_solo(self):
-      solos = ''
-      for each_solo in self.members:
-        solos += f"{each_solo.play_solo()}\n"
-        return solos
-      # return f"{self.name} is playing solo on the {self.instrument}"
+      return f"My name is {self.name} and I play {self.instrument}" 
     
     def get_instrument(self):
       return f"{self.instrument}"
-      
-# @staticmethod
-# def new_data(data):
-#   data_split = re.findall(r"\S.*", data)
-#   print(data)
-  
+     
+    def play_solo(self):
+      return f"{self.solo}"
+
 
 class Guitarist(Musician):
   def __init__(self, name):
-    super().__init__(name,"Guitarist", "guitar")
+    super().__init__(name,"Guitarist", "guitar", "face melting guitar solo")
+    
 
 class Bassist(Musician):
-   def __init__(self, name):
-      super().__init__(name,"Bassist", "bass")
+  def __init__(self, name):
+    super().__init__(name,"Bassist", "bass", "bom bom buh bom")
+      
  
 class Drummer(Musician):
-   def __init__(self, name):
-      super().__init__(name,"Drummer", "drums")
-     
+  def __init__(self, name):
+    super().__init__(name,"Drummer", "drums", "rattle boom crash")
   
 
-if __name__ == "__main__":
-  x = Musician('C')
-  print(x.play_solo)
+
   
-  band = """
-  The Roots
-  AOwen Bibble, Guitarist
-  Mark Kelly, Bass
-  Quest Love, Drums
-  """ 
+  
